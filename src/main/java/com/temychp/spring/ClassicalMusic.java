7bfc1c33f1323cf0a3effe9019e98ea5533273c1
temychpp <temychp@gmail.com>
2023-02-09 14:52:55 +0300

    update 09les

--- src/main/java/com/temychp/spring/ClassicalMusic.java
+++ src/main/java/com/temychp/spring/ClassicalMusic.java
@@ -2,6 +2,21 @@ package com.temychp.spring;
 
 public class ClassicalMusic implements Music {
 
+
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Vivaldi";
--- src/main/java/com/temychp/spring/TestSpring.java
+++ src/main/java/com/temychp/spring/TestSpring.java
@@ -7,14 +7,19 @@ public class TestSpring {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
+
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+
+
 //        Music music = context.getBean("musicBean", Music.class);
 //
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        //   System.out.println(testBean.getName());
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+//        System.out.println(testBean.getName());
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
