9b1eac520d60bfcf56fc8a0beb6509368fdaabc5
temychpp <temychp@gmail.com>
2023-02-13 10:40:40 +0300

    update 12les DI qualifier1

--- src/main/java/com/temychp/spring/ClassicalMusic.java
+++ src/main/java/com/temychp/spring/ClassicalMusic.java
@@ -6,19 +6,19 @@ import org.springframework.stereotype.Component;
 public class ClassicalMusic implements Music {
 
 
-    public ClassicalMusic() {}
+//    public ClassicalMusic() {}
 
-    public static ClassicalMusic getClassicalMusic(){
-        return new ClassicalMusic();
-    }
-
-    public void doMyInit(){
-        System.out.println("Doing my initialization");
-    }
-
-    public void doMyDestroy(){
-        System.out.println("Doing my destruction");
-    }
+//    public static ClassicalMusic getClassicalMusic(){
+//        return new ClassicalMusic();
+//    }
+//
+//    public void doMyInit(){
+//        System.out.println("Doing my initialization");
+//    }
+//
+//    public void doMyDestroy(){
+//        System.out.println("Doing my destruction");
+//    }
 
     @Override
     public String getSong() {
--- src/main/java/com/temychp/spring/MusicPlayer.java
+++ src/main/java/com/temychp/spring/MusicPlayer.java
@@ -1,21 +1,24 @@
 package com.temychp.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
 @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
+@Qualifier ("classicalMusic")
+    private Music music;
+//    private ClassicalMusic classicalMusic;
+//    private RockMusic rockMusic;
+//@Autowired
+//    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+//        this.classicalMusic = classicalMusic;
+//        this.rockMusic = rockMusic;
+//    }
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong()+ " " + rockMusic.getSong();
+        return "Playing: " + music.getSong();
     }
 
 
