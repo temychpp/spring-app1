8d2e039c03c63195b8032a9c4e9b22b1bb239d2d
temychpp <temychp@gmail.com>
2023-02-13 10:09:17 +0300

    update 11les DI

--- src/main/java/com/temychp/spring/MusicPlayer.java
+++ src/main/java/com/temychp/spring/MusicPlayer.java
@@ -1,59 +1,23 @@
 package com.temychp.spring;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class MusicPlayer {
-//    private List<Music> musicList = new ArrayList<>();
 
-    private Music music;
-    private String name;
-    private int volume;
-
-    public String getName() {
-        return name;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
+@Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong()+ " " + rockMusic.getSong();
     }
 
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-//    public MusicPlayer(List<Music> musicList){
-//        this.musicList = musicList;
-//    }
-
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
-
-    public MusicPlayer() {
-
-    }
-
-//    public void setMusicList(List<Music> musicList){
-//        this.musicList=musicList;
-//    }
-
-    public void setMusic(Music music) {
-        this.music = music;
-    }
-
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
-    }
-
-//public void playMusic(){
-//        for(Music music: musicList)
-//    System.out.println("Playing: " + music.getSong());
-//}
 
 
 }
--- src/main/java/com/temychp/spring/TestSpring.java
+++ src/main/java/com/temychp/spring/TestSpring.java
@@ -12,18 +12,21 @@ public class TestSpring {
       //  System.out.println(classicalMusic.getSong());
 
 
-        Music music = context.getBean("rockMusic", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
-        Music music2 = context.getBean("classicalMusic", Music.class);
-        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
-//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        musicPlayer2.playMusic();
+//        Music music = context.getBean("rockMusic", Music.class);
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
+//
+//        Music music2 = context.getBean("classicalMusic", Music.class);
+//        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+////        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+//        musicPlayer2.playMusic();
 
 //        System.out.println(testBean.getName());
 //        System.out.println(musicPlayer.getName());
 //        System.out.println(musicPlayer.getVolume());
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
