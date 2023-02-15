eeecaaaa3f4588687675bccf935d1754e5d6e10f
temychpp <temychp@gmail.com>
2023-02-15 17:03:56 +0300

    Lesson_19_SpringRequestParameters up2

--- src/Lesson_19_SpringRequestParameters/src/main/java/com/temychp/spring/controllers/FirstController.java
+++ src/Lesson_19_SpringRequestParameters/src/main/java/com/temychp/spring/controllers/FirstController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.HttpSessionRequiredException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -13,12 +14,10 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(HttpServletRequest request) {
-        String name = request.getParameter("name");
-        String surname = request.getParameter("surname");
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
         System.out.println("Hello, " + name + " " + surname);
-
-
 
         return "first/hello";
     }
