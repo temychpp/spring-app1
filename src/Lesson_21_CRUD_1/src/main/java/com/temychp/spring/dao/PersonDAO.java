4854db9f7280c956445f1f52ef9fd892c696c4ab
temychpp <temychp@gmail.com>
2023-02-17 11:41:32 +0300

    Lesson_21_crud add

---
+++ src/Lesson_21_CRUD_1/src/main/java/com/temychp/spring/dao/PersonDAO.java
@@ -0,0 +1,49 @@
+package com.temychp.spring.dao;
+
+import com.temychp.spring.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+        people.add(new Person(++PEOPLE_COUNT, "Tem"));
+    }
+
+
+    public List<Person> index() {
+        return people;
+    }
+
+//    public Person show(int id) {
+//        return people.stream()
+//                .filter(person -> person.getId() == id)
+//                .findAny().orElse(null);
+//    }
+
+    public Person show(int id) {
+        for (Person person:people) {
+            if(person.getId() == id){
+                return person;
+            }
+        }
+
+        return null;
+    }
+
+
+
+
+
+}
