584c6af1d08d7ee6452146482ce167dd13aab2be
temychpp <temychp@gmail.com>
2023-02-18 11:48:50 +0300

    Lesson_23_crud_update up

--- src/Lesson_21_CRUD_1/src/main/java/com/temychp/spring/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ src/Lesson_21_CRUD_1/src/main/java/com/temychp/spring/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -7,6 +7,10 @@ import javax.servlet.Servlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
+
+
+
+
 public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
@@ -30,9 +34,9 @@ public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotation
         registerHiddenFieldFilter(aServletContext);
     }
 
-
-    public void registerHiddenFieldFilter(ServletContext aContext) {
-        aContext.addFilter("hiddenHttpMethodFilter", new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 
 
