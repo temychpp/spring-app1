9b8b7a1cd9a7b37d2d751e3ad9de92a5d0df65d6
temychpp <temychp@gmail.com>
2023-02-19 12:40:40 +0300

    Lesson_24_formValidator starter up

--- src/Lesson_24_SpringFormValidation/src/main/java/com/temychp/spring/controllers/PeopleController.java
+++ src/Lesson_24_SpringFormValidation/src/main/java/com/temychp/spring/controllers/PeopleController.java
@@ -2,8 +2,10 @@ package com.temychp.spring.controllers;
 
 import com.temychp.spring.dao.PersonDAO;
 import com.temychp.spring.models.Person;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -30,30 +32,37 @@ public class PeopleController {
     }
 
     @GetMapping("/new")
-    public String newPerson(@ModelAttribute Person person) {
+    public String newPerson(@ModelAttribute("person") Person person) {
         return "people/new";
     }
 
     @PostMapping()
-    public String create(@ModelAttribute Person person){
-personDAO.save(person);
+    public String create(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "people/new";
+        personDAO.save(person);
         return "redirect:/people";
     }
 
 
     @GetMapping("/{id}/edit")
-    public String edit(Model model, @PathVariable("id") int id){
-        model.addAttribute("person",personDAO.show(id));
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("person", personDAO.show(id));
         return "people/edit";
     }
 
     @PatchMapping("/{id}")
-    public String update (@ModelAttribute("person") Person person,@PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
-        return  "redirect:/people";
+        return "redirect:/people";
     }
+
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") int id){
+    public String delete(@PathVariable("id") int id) {
         personDAO.delete(id);
         return "redirect:/people";
     }
--- src/Lesson_24_SpringFormValidation/src/main/java/com/temychp/spring/models/Person.java
+++ src/Lesson_24_SpringFormValidation/src/main/java/com/temychp/spring/models/Person.java
@@ -1,16 +1,23 @@
 package com.temychp.spring.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class Person {
 
     private int id;
-
+    @NotEmpty(message = "name should not be empty")
+    @Size(min = 2, max = 30, message = "name should be between 2 and 30 characters")
     private String name;
-
+    @Min(value = 0, message = "Age should be greater than 0")
     private int age;
-
+    @NotEmpty(message = "email should not be empty")
+    @Email(message = "email should be valid")
     private String email;
 
-    public Person(){
+    public Person() {
 
     }
 
