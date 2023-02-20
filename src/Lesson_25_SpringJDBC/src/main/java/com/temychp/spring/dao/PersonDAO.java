13fca8c376a87a828ccf136bbf8d8ece4a3a0815
temychpp <temychp@gmail.com>
2023-02-20 11:51:30 +0300

    Lesson_25_JDBC up1

--- src/Lesson_25_SpringJDBC/src/main/java/com/temychp/spring/dao/PersonDAO.java
+++ src/Lesson_25_SpringJDBC/src/main/java/com/temychp/spring/dao/PersonDAO.java
@@ -3,6 +3,7 @@ package com.temychp.spring.dao;
 import com.temychp.spring.models.Person;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,44 +11,97 @@ import java.util.List;
 public class PersonDAO {
 
     private static int PEOPLE_COUNT;
-    private List<Person> people;
 
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Tom",22,"tom@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Bob",25,"bob@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Mike",30,"mike@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Katy",18,"katy@mail.ru"));
-        people.add(new Person(++PEOPLE_COUNT, "Tem",35,"tem@gmail.com"));
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
 
+//    private List<Person> people;
+//
+//    {
+//        people = new ArrayList<>();
+//        people.add(new Person(++PEOPLE_COUNT, "Tom",22,"tom@gmail.com"));
+//        people.add(new Person(++PEOPLE_COUNT, "Bob",25,"bob@mail.ru"));
+//        people.add(new Person(++PEOPLE_COUNT, "Mike",30,"mike@gmail.com"));
+//        people.add(new Person(++PEOPLE_COUNT, "Katy",18,"katy@mail.ru"));
+//        people.add(new Person(++PEOPLE_COUNT, "Tem",35,"tem@gmail.com"));
+//    }
+
+
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            while (resultSet.next()) {
+                Person person = new Person();
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
         return people;
     }
 
     public Person show(int id) {
-        return people.stream()
-                .filter(person -> person.getId() == id)
-                .findAny().orElse(null);
+//        return people.stream()
+//                .filter(person -> person.getId() == id)
+//                .findAny().orElse(null);
+        return null;
     }
-
 
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+//        person.setId(++PEOPLE_COUNT);
+//        people.add(person);
+
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "INSERT INTO Person VALUES(" + 1 + ",'" + person.getName() + "'," +
+                    person.getAge() + ",'" + person.getEmail() + "')";
+            statement.executeUpdate(SQL);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
     }
 
     public void update(int id, Person updatePerson) {
-        Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatePerson.getName());
-        personToBeUpdated.setAge(updatePerson.getAge());
-        personToBeUpdated.setEmail(updatePerson.getEmail());
+//        Person personToBeUpdated = show(id);
+//        personToBeUpdated.setName(updatePerson.getName());
+//        personToBeUpdated.setAge(updatePerson.getAge());
+//        personToBeUpdated.setEmail(updatePerson.getEmail());
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+//        people.removeIf(p -> p.getId() == id);
     }
 }
 
