d04f7635c28e675b8cfb2a85ae1bd97008d60992
temychpp <temychp@gmail.com>
2023-03-06 14:32:34 +0300

    hibernateApp oneToMany add

--- src/HibernateApp/src/main/java/org/example/App.java
+++ src/HibernateApp/src/main/java/org/example/App.java
@@ -4,29 +4,27 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
+
 
         try {
             session.beginTransaction();
 
 
-            session.createQuery("UPDATE Person set name='Test' where age<30").executeUpdate();
 
-//            for (Person person:people
-//                 ) {
-//                System.out.println(person);
-//                            }
-
-            System.out.println();
             session.getTransaction().commit();
         } finally {
             sessionFactory.close();
         }
+
     }
+
 }
