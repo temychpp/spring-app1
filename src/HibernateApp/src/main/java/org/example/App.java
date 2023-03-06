35c0dfe794770b5d718ac67ee55f591764764350
temychpp <temychp@gmail.com>
2023-03-06 16:53:29 +0300

    (cherry picked from commit a378d053a01b01447c245c529de25787bc22ed21)

--- src/HibernateApp/src/main/java/org/example/App.java
+++ src/HibernateApp/src/main/java/org/example/App.java
@@ -1,61 +1,33 @@
 package org.example;
 
-import org.example.model.Actor;
-import org.example.model.Movie;
+import org.example.model.Item;
+import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().
-                addAnnotatedClass(Movie.class).
-                addAnnotatedClass(Actor.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
 
-        try (sessionFactory) {
-            Session session = sessionFactory.getCurrentSession();
+
+        try {
             session.beginTransaction();
-//1 добавляем фильм и двух актеров из этого фильма
-//            Movie movie = new Movie("Pulp fiction", 1994);
-//            Actor actor1 = new Actor("Harvey Keitel", 81);
-//            Actor actor2 = new Actor("Samuel L. Jackson", 72);
-//            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
-//            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-//            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-//            session.save(movie);
-//            session.save(actor1);
-//            session.save(actor2);
 
-// 2 узнаем актеров из фильма и фильм у актеров
-//            Movie movie = session.get(Movie.class,1);
-//            System.out.println(movie.getActors());
-//
-//            Actor actor1 = session.get(Actor.class, 1);
-//            Actor actor2 = session.get(Actor.class, 2);
-//            System.out.println(actor1.getMovies());
-//            System.out.println(actor2.getMovies());
+            Person person = new Person("Test Cascading", 33);
+            Item item = new Item("Test Cascading Item", person);
+            person.setItems(new ArrayList<>(Collections.singletonList(item)));
 
-//  3 добавили фильм и назначили ему связь с актером
-//            Movie movie = new Movie("Reservoir Dogs", 1992);
-//            Actor actor = session.get(Actor.class, 1);
-//            movie.setActors(new ArrayList<>(Collections.singletonList(actor)));
-//            actor.getMovies().add(movie);
-//            session.save(movie);
-
-//  4  удаляем фильм у актера
-            Actor actor = session.get(Actor.class, 2);
-            Movie movieToRemove = actor.getMovies().get(0);
-
-            actor.getMovies().remove(0);
-            movieToRemove.getActors().remove(actor);
+            session.persist(person);
 
             session.getTransaction().commit();
+        } finally {
+            sessionFactory.close();
         }
 
     }
