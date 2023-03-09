3e0ebc9c7632ce80fa4da0d47fa7c4b36c176c16
temychpp <temychp@gmail.com>
2023-03-09 18:20:34 +0300

    hibernate_one_to_one имя человека по паспорту

--- src/HibernateApp/src/main/java/org/example/App.java
+++ src/HibernateApp/src/main/java/org/example/App.java
@@ -17,12 +17,15 @@ public class App {
         try {
             session.beginTransaction();
 
-            Person person = new Person("Test person", 50);
-            Passport passport = new Passport(123456);
+//1 создать человека и паспорт
+//            Person person = new Person("Test person", 50);
+//            Passport passport = new Passport(123456);
+//            person.setPassport(passport);
+//            session.save(person);
 
-            person.setPassport(passport);
-
-            session.save(person);
+//2 имя человека по id паспорта
+            Passport passport = session.get(Passport.class,1);
+            System.out.println(passport.getPerson().getName());
 
             session.getTransaction().commit();
         } finally {
