caa71dc5d4373f56b574961fc5d053f083e8b42d
temychpp <temychp@gmail.com>
2023-03-10 13:02:53 +0300

    hibernate_many_to_many // 2 узнаем актеров из фильма и фильм у актеров

--- src/HibernateApp/src/main/java/org/example/App.java
+++ src/HibernateApp/src/main/java/org/example/App.java
@@ -6,6 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -21,17 +22,27 @@ public class App {
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 //1 добавляем фильм и двух актеров из этого фильма
-            Movie movie = new Movie("Pulp fiction", 1994);
-            Actor actor1 = new Actor("Harvey Keitel", 81);
-            Actor actor2 = new Actor("Samuel L. Jackson", 72);
-            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
-            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
-            session.save(movie);
-            session.save(actor1);
-            session.save(actor2);
+//            Movie movie = new Movie("Pulp fiction", 1994);
+//            Actor actor1 = new Actor("Harvey Keitel", 81);
+//            Actor actor2 = new Actor("Samuel L. Jackson", 72);
+//            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
+//            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+//            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+//            session.save(movie);
+//            session.save(actor1);
+//            session.save(actor2);
 
-            session.getTransaction().commit();
+// 2 узнаем актеров из фильма и фильм у актеров
+            Movie movie = session.get(Movie.class,1);
+            System.out.println(movie.getActors());
+
+            Actor actor1 = session.get(Actor.class, 1);
+            Actor actor2 = session.get(Actor.class, 2);
+            System.out.println(actor1.getMovies());
+            System.out.println(actor2.getMovies());
+
+
+                    session.getTransaction().commit();
         }
 
     }
