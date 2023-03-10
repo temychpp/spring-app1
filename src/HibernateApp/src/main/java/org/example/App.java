512aee02ca84c7c35b67375b1f9ea4cffc798112
temychpp <temychp@gmail.com>
2023-03-10 12:57:12 +0300

    hibernate_many_to_many добавлен фильм и актеры из этого фильма

--- src/HibernateApp/src/main/java/org/example/App.java
+++ src/HibernateApp/src/main/java/org/example/App.java
@@ -6,6 +6,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration().
@@ -13,10 +17,19 @@ public class App {
                 addAnnotatedClass(Actor.class);
         SessionFactory sessionFactory = configuration.buildSessionFactory();
 
-        try (sessionFactory){
+        try (sessionFactory) {
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
-
+//1 добавляем фильм и двух актеров из этого фильма
+            Movie movie = new Movie("Pulp fiction", 1994);
+            Actor actor1 = new Actor("Harvey Keitel", 81);
+            Actor actor2 = new Actor("Samuel L. Jackson", 72);
+            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
+            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+            session.save(movie);
+            session.save(actor1);
+            session.save(actor2);
 
             session.getTransaction().commit();
         }
