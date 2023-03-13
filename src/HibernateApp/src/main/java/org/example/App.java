1677ae2f7ae9d10252bc250db3b4d9d6c712d1be
temychpp <temychp@gmail.com>
2023-03-13 10:12:49 +0300

    one_to_many lazy 2 получаем товар и человека

--- src/HibernateApp/src/main/java/org/example/App.java
+++ src/HibernateApp/src/main/java/org/example/App.java
@@ -22,10 +22,17 @@ public class App {
         try {
             session.beginTransaction();
 //1 получаем человека и связанные сущности (Lazy)
-            Person person = session.get(Person.class, 1);
-            System.out.println("Get person id=1");
-            //Получаем связанные сущности (Lazy)
-            System.out.println(person.getItems());
+//            Person person = session.get(Person.class, 1);
+//            System.out.println("Get person id=1");
+//            //Получаем связанные сущности (Lazy)
+//            System.out.println(person.getItems());
+
+//2 получаем товар и человека
+            Item item = session.get(Item.class, 1);
+            System.out.println("Get item id=1");
+            System.out.println(item.getOwner());
+
+
 
             session.getTransaction().commit();
             // session.close()
