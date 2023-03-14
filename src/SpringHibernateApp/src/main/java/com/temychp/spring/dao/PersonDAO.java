bfcb05c2245c2da3cd674741ce9f1873ad19eb5d
temychpp <temychp@gmail.com>
2023-03-14 10:19:54 +0300

    spring hibernate app add fix Transactional

--- src/SpringHibernateApp/src/main/java/com/temychp/spring/dao/PersonDAO.java
+++ src/SpringHibernateApp/src/main/java/com/temychp/spring/dao/PersonDAO.java
@@ -5,8 +5,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Component
@@ -19,35 +19,34 @@ public class PersonDAO {
         this.sessionFactory = sessionFactory;
     }
 
-    @Transactional()
+    @Transactional(readOnly = true)
     public List<Person> index() {
         Session session = sessionFactory.getCurrentSession();
         return session.createQuery("select p from Person p", Person.class).getResultList();
     }
 
-    @Transactional()
+    @Transactional(readOnly = true)
     public Person show(int id) {
         Session session = sessionFactory.getCurrentSession();
         return session.get(Person.class, id);
     }
 
-    @Transactional()
+    @Transactional
     public void save(Person person) {
         Session session = sessionFactory.getCurrentSession();
         session.save(person);
     }
 
-    @Transactional()
+    @Transactional
     public void update(int id, Person updatedPerson) {
         Session session = sessionFactory.getCurrentSession();
         Person updatePerson = session.get(Person.class, id);
         updatePerson.setName(updatedPerson.getName());
         updatePerson.setAge(updatedPerson.getAge());
         updatePerson.setEmail(updatedPerson.getEmail());
-
     }
 
-    @Transactional()
+    @Transactional
     public void delete(int id) {
         Session session = sessionFactory.getCurrentSession();
         session.delete(session.get(Person.class, id));
