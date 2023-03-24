332db4c23467329bf164a0f21f8f065c1a42edd4
temychpp <temychp@gmail.com>
2023-03-24 14:46:21 +0300

    spring security authentication 4

--- src/SpringSecurityApp/src/main/java/com/temychp/SpringSecurity1/controllers/HelloController.java
+++ src/SpringSecurityApp/src/main/java/com/temychp/SpringSecurity1/controllers/HelloController.java
@@ -1,5 +1,11 @@
 package com.temychp.SpringSecurity1.controllers;
 
+import com.temychp.SpringSecurity1.details.PersonDetails;
+import com.temychp.SpringSecurity1.repositories.PeopleRepository;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,9 +13,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String sayHello(){
+    public String sayHello() {
         return "hello";
     }
 
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+        return "hello";
+    }
 }
