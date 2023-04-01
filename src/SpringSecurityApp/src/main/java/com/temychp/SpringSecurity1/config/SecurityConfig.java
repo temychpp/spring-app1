a2eed64cd5b08114997ac2e685c18d52b0383ae0
temychpp <temychp@gmail.com>
2023-04-01 13:21:25 +0300

    spring security авторизация. аннотация @PreAuthorize

--- src/SpringSecurityApp/src/main/java/com/temychp/SpringSecurity1/config/SecurityConfig.java
+++ src/SpringSecurityApp/src/main/java/com/temychp/SpringSecurity1/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.temychp.SpringSecurity1.Services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PersonDetailsService personDetailsService;
@@ -26,7 +28,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().hasAnyRole("USER","ADMIN")
                 .and()
--- src/SpringSecurityApp/src/main/java/com/temychp/SpringSecurity1/controllers/HelloController.java
+++ src/SpringSecurityApp/src/main/java/com/temychp/SpringSecurity1/controllers/HelloController.java
@@ -1,16 +1,22 @@
 package com.temychp.SpringSecurity1.controllers;
 
+import com.temychp.SpringSecurity1.Services.AdminService;
 import com.temychp.SpringSecurity1.details.PersonDetails;
-import com.temychp.SpringSecurity1.repositories.PeopleRepository;
-import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
     @GetMapping("/hello")
     public String sayHello() {
@@ -27,8 +33,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
-
-
 }
