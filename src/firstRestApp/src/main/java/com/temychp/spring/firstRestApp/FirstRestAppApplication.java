9bb2e56db960a907516d4fd753c3f4253875e223
temychpp <temychp@gmail.com>
2023-04-04 12:23:47 +0300

    first REST API start

---
+++ src/firstRestApp/src/main/java/com/temychp/spring/firstRestApp/FirstRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.temychp.spring.firstRestApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstRestAppApplication.class, args);
+	}
+
+}
